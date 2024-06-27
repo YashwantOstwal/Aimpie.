d193651e6de1c40e275e196c3f3cc5ac9df357e5
Yashwant Ostwal <140978232+YashwantOstwal@users.noreply.github.com>
2024-06-28 00:36:23 +0530

    Update WorkMarquee.jsx

--- src/pages/Homepage/WorkMarquee.jsx
+++ src/pages/Homepage/WorkMarquee.jsx
@@ -4,10 +4,10 @@ export default function WorkMarquee (){
     return (
         <div className = 'space-y-4 bg-black py-[2%]'>
             <Marquee direction = 'left' gradient = {true} gradientColor='black' gradientWidth = {250} autoFill={true} >
-                <img className = 'opacity-10 max-md:opacity-10' src={ourSelectedWork}  />
+                <img className = 'opacity-10 max-md:opacity-30' src={ourSelectedWork}  />
             </Marquee>
             <Marquee direction = 'right' gradient = {true} gradientColor = 'black' gradientWidth = {250} speed = {90} autoFill={true} className = 'bg-black '>
-                <img className = 'opacity-10 max-md:opacity-10' src={ourSelectedWork}  />
+                <img className = 'opacity-10 max-md:opacity-30' src={ourSelectedWork}  />
             </Marquee>
         </div>
     );
